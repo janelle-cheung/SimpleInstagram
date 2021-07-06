b26cbb5ebc6c85ce3e7ec1b584b1d3d8bb479ef9
janelle-cheung <cheungj19@gmail.com>
2021-07-06 18:32:08 -0400

    Created login activity, setup view binding

---
+++ app/src/main/java/com/example/simpleinstagram/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.simpleinstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.simpleinstagram.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        
+    }
+}
--- app/src/main/java/com/example/simpleinstagram/MainActivity.java
+++ app/src/main/java/com/example/simpleinstagram/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.simpleinstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.simpleinstagram.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
